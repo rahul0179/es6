a4edc083e5fef3de77cf1f8d6efcd6fec3b46154
Rahul barman <75018120+rahul0179@users.noreply.github.com>
2022-08-09 00:51:08 +0600

    array function

---
+++ funtion.js
@@ -0,0 +1,14 @@
+const add = (num1, num2) => num1 + num2;
+//console.log(add(15, 15));
+const multiply = (n1, n2, n3) => n1 * n2 * n3;
+let result = multiply(5, 5, 4)
+//console.log(result)
+
+const math = (x, y) => {
+    const sum = x + y;
+    const sun = x - y;
+    const result = sum * sun;
+    return result;
+}
+const resul = math(15, 10);
+console.log(resul);
